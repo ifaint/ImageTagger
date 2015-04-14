15839b237308d0511e3813c6c00ecf8ded67dd20
tainy-zhang <xiaofei.zhang@eventbank.com>
2015-04-14 16:00:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tainy/imagetagger/TagActivity.java
@@ -0,0 +1,101 @@
+package com.example.tainy.imagetagger;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.EditText;
+
+
+public class TagActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tag);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_tag, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState)
+        {
+            View rootView = inflater.inflate(R.layout.fragment_tag, container, false);
+            Log.d("in TagActivity","onCreateView");
+            return rootView;
+        }
+
+        @Override
+        public void onResume()
+        {
+            super.onResume();
+            Intent intent = this.getActivity().getIntent();
+            AlertDialog.Builder builder = new AlertDialog.Builder(this.getActivity());
+            LayoutInflater inflater = LayoutInflater.from(this.getActivity());
+            View view = inflater.inflate(R.layout.tag_input_view,null);
+            final EditText et_tag = (EditText) view.findViewById(R.id.tag_name);
+            builder.setTitle("input the tag");
+            builder.setView(view);
+            builder.setNegativeButton(android.R.string.cancel, null);
+            builder.setPositiveButton(android.R.string.ok,new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    String tag = et_tag.getText().toString();
+                    saveTag(tag);
+                }
+            });
+            builder.create().show();
+        }
+
+        private void saveTag(String tag)
+        {
+
+        }
+    }
+}
